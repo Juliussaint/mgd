6980728ec56edaf3f64b35c9af197997a2e9ec88
Juliussaint <yuliussiregar@gmail.com>
2026-02-11 14:28:03 +0700

    update isi entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
+set -x   # show commands
 set -e
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py migrate --noinput || echo "MIGRATE FAILED"
+python manage.py collectstatic --noinput || echo "STATIC FAILED"
 
-exec gunicorn config.wsgi:application \
-  --bind 0.0.0.0:8000 \
-  --workers "${GUNICORN_WORKERS:-3}" \
-  --timeout "${GUNICORN_TIMEOUT:-120}" \
-  --access-logfile - \
-  --error-logfile - \
-  --log-level info \
-  --capture-output
+gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000
