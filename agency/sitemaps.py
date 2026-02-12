089f3f1a7a5a9f4268de94590b8e6744f0e7fa64
Juliussaint <yuliussiregar@gmail.com>
2026-02-12 13:56:43 +0700

    update sitemaps lagi

--- agency/sitemaps.py
+++ agency/sitemaps.py
@@ -1,7 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from agency.models import Project
-from blog.models import Post 
-
+from blog.models import Post
 
 class StaticViewSitemap(Sitemap):
     changefreq = 'monthly'
@@ -10,16 +9,20 @@ class StaticViewSitemap(Sitemap):
 class ProjectSitemap(Sitemap):
     changefreq = 'weekly'
     priority = 0.8
+    
     def items(self):
-        return Project.objects.filter(status='title')
+        return Project.objects.all()
+    
     def lastmod(self, obj):
         return obj.created_at
 
 class BlogSitemap(Sitemap):
     changefreq = 'daily'
     priority = 0.6
+    
     def items(self):
-        # Mengambil Post dari model blog
+        
         return Post.objects.filter(status='published')
+    
     def lastmod(self, obj):
         return obj.updated_at
